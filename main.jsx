d3e0380363533c420396b0079603046100f4fe47
AlexMetatron <alexanderllanas0@gmail.com>
2025-05-26 19:13:02 -0600

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => (
+  <h1 style={{ color: 'green' }}>Bienvenido a MetaGreen 🌿</h1>
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
